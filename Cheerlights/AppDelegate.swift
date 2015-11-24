8a78f4a944cbf980bd990333f7ab704b413073de
Kilian Koeltzsch <me@kilian.io>
2015-11-24 11:16:36 +0100

    Initial Commit

---
+++ Cheerlights/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Cheerlights
+//
+//  Created by Kilian Költzsch on 24/11/15.
+//  Copyright © 2015 Kilian Költzsch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
